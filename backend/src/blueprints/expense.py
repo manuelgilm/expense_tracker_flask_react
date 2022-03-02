2c86f912a26d7d6a87d044fbe0243744c67bec28
Manuel Gil <manuelgilsitio@gmail.com>
2022-03-02 00:06:26 -0500

    endpoints have been updated

--- backend/src/blueprints/expense.py
+++ backend/src/blueprints/expense.py
@@ -1,8 +1,10 @@
+from http.client import ResponseNotReady
 import json
 
 from click import get_current_context
 from src.db import db
 from flask import Blueprint, jsonify, request
+from src import responses
 from src.models.expense import ExpenseModel
 from src.schemas.expense import ExpenseSchema
 from src.models.category import CategoryModel
@@ -17,15 +19,15 @@ expense_schema = ExpenseSchema()
 @jwt_required()
 def get_expenses():
     expenses = ExpenseModel.find_expenses_by_owner(get_jwt_identity())
-    return jsonify({"response":expense_schema.dump(expenses, many = True)})
+    return jsonify(expense_schema.dump(expenses, many = True)), responses.HTTP_200_OK
 
 @exp.get("/<int:id>")
 @jwt_required()
 def get_expense(id):
     expense = ExpenseModel.find_expense_by_id_and_owner(expense_id=id, user_id=get_jwt_identity())
     if expense:
-        return jsonify({"response":expense_schema.dump(expense)})
-    return jsonify({"response":"Expense not found!"})
+        return jsonify(expense_schema.dump(expense)), responses.HTTP_200_OK
+    return jsonify(response = responses.ELEMENT_NOT_FOUND.format("Expense")), responses.HTTP_404_NOT_FOUND
 
 @exp.post("/create")
 @jwt_required()
@@ -39,8 +41,8 @@ def create_expense():
         data.update({"user_id":current_user_id,"category_id":category.id})
         expense = expense_schema.load(data)
         expense.save_to_db()
-        return jsonify({"response":"expense has been created"})
-    return jsonify({"response":"Category not Found"}), 404
+        return jsonify(response = responses.ELEMENT_CREATED.format("Expense")), responses.HTTP_201_CREATED
+    return jsonify(response = responses.ELEMENT_NOT_FOUND.format("Category")), responses.HTTP_400_BAD_REQUEST
 
 
 @exp.delete("/<int:id>")
@@ -50,17 +52,17 @@ def delete_expense(id):
     expenses = ExpenseModel.find_expenses_by_owner(user_id=get_jwt_identity())
     if expense in expenses:
         expense.delete_from_db()
-        return jsonify({"response":"expense has been deleted"})
-    return jsonify({"response":"expense not found!"})
+        return jsonify(response = responses.DELETED.format("Expense")), responses.HTTP_204_NO_CONTENT
+    return jsonify(response = responses.ELEMENT_NOT_FOUND.format("Expense")), responses.HTTP_404_NOT_FOUND
 
 @exp.get("/total")
 @jwt_required()
 def total():
     expenses = ExpenseModel.total_expenses_by_user_id(user_id=get_jwt_identity())
-    return jsonify({"response":expenses[0]})
+    return jsonify(response = expenses[0]), responses.HTTP_200_OK
 
 @exp.get("/total/<int:category_id>")
 @jwt_required()
 def total_by_category(category_id):
     expenses_category = ExpenseModel.total_expenses_by_category(user_id=get_jwt_identity(), category_id=category_id)
-    return jsonify({"response":expenses_category[0]})
+    return jsonify(response = expenses_category[0])
--- backend/src/blueprints/user.py
+++ backend/src/blueprints/user.py
@@ -1,8 +1,10 @@
 from gettext import gettext
+import json
 
 from src.db import db
 from flask import Blueprint, request, jsonify
 from marshmallow import ValidationError
+from src import responses
 from src.models.user import UserModel
 from src.schemas.user import UserSchema
 from src.ma import ma
@@ -17,7 +19,7 @@ from flask_jwt_extended import (
 )
 
 user = Blueprint(name="users", import_name=__name__)
-#---------------USER CRUD-----------------
+#---------------USER ENDPOINTS-----------------
 
 user_schema = UserSchema()
 
@@ -26,17 +28,18 @@ user_schema = UserSchema()
 def get_users():
     users = UserModel.find_users()
     users = user_schema.dump(users, many=True)
-    return jsonify(users)
+    return jsonify(users), responses.HTTP_200_OK
 
 @user.post("/create")
-@jwt_required()
+# @jwt_required()
 def create_user():
     try:
         user = user_schema.load(request.get_json(),session=db.session)
     except ValidationError as err:
-        return err.messages, 400
+        return err.messages, responses.HTTP_400_BAD_REQUEST
+
     user.save_to_db()
-    return jsonify({"response":"user created"})
+    return jsonify(response = responses.ELEMENT_CREATED.format("User")) , responses.HTTP_201_CREATED
 
 @user.get("/<int:id>")
 @jwt_required()
@@ -49,29 +52,28 @@ def get_user_by_id(id):
 def delete_user(id):
     user = UserModel.find_by_id(id)
     user.delete_from_db()
-    return jsonify({"response":"user has been deleted"})
+    return jsonify(response= responses.DELETED.format("User")) , responses.HTTP_204_NO_CONTENT
 
 @user.post("/login")
 def login():
-    user_json = request.get_json()
-    user_obj = user_schema.load(user_json)
-
+    user_obj = user_schema.load(request.get_json())
     user = UserModel.find_by_username(user_obj.username)
-
-    if user and safe_str_cmp(user.password, user_obj.password):
-        access_token = create_access_token(identity=user.id, fresh=True)
-        refresh_token = create_refresh_token(user.id)
-        return {"access_token": access_token, "refresh_token": refresh_token}, 200
-
-    return {"response":"Invalid credentials"}, 401
+    if user:
+        if safe_str_cmp(user.password, user_obj.password):
+            access_token = create_access_token(identity=user.id, fresh=True)
+            refresh_token = create_refresh_token(user.id)
+            return jsonify(access_token = access_token, refresh_token = refresh_token), responses.HTTP_200_OK
+    else: 
+        return jsonify(response = responses.ELEMENT_NOT_FOUND.format("User")), responses.HTTP_404_NOT_FOUND
+    return jsonify(response = responses.INVALID_CREDENTIALS) , responses.HTTP_401_UNAUTHORIZED
 
 @user.post("/logout")
 @jwt_required()
 def post():
-    jti = get_jwt()["jti"]  # jti is "JWT ID", a unique identifier for a JWT.
+    jti = get_jwt()["jti"]
     user_id = get_jwt_identity()
     BLOCKLIST.add(jti)
-    return {"message": "user_logged_out"}, 200
+    return jsonify(response = responses.LOGOUT), responses.HTTP_200_OK
 
 
 
